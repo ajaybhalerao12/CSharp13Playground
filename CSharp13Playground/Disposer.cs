5f58c777fe1bacf17b5a4825b93299922d1a6a39
ajaybhalerao12 <ajaybhalerao12@gmail.com>
2024-12-24 11:23:35 +0530

    Add project files.

---
+++ CSharp13Playground/Disposer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Immutable;
+
+namespace CSharp13Playground
+{
+    internal static class Disposer
+    {
+        public static void DisposeAll<T>(ImmutableArray<T> disposables)
+                 where T : IDisposable
+        {
+            foreach (var disposable in disposables) { 
+                disposable.Dispose();
+            }
+        }
+
+        public static void DisposeAll<T>(IEnumerable<T> disposables)
+            where T : IDisposable
+        {
+            foreach (var disposable in disposables)
+            {
+                disposable.Dispose();
+            }
+        }
+
+        public static void DisposeAll<T>(params ReadOnlySpan<T> disposables)
+           where T : IDisposable
+        {
+            foreach (var disposable in disposables)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
---
+++ CSharp13Playground/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSharp13Playground;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+// 1. Params collection enhancements
+Disposer.DisposeAll<StringReader>(new("Hello"), new("World"));
+
+
